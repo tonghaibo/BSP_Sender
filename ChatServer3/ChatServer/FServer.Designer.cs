fc364dcd399fbbc963baa6780562ead00043bad1
chengwengao <657325704@qq.com>
2017-08-21 13:23:13 +0800

    首次提交

---
+++ ChatServer3/ChatServer/FServer.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace ChatServer
+{
+    partial class FServer
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSendMsg = new System.Windows.Forms.TextBox();
+            this.btnSendMsg = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnStartService = new System.Windows.Forms.Button();
+            this.lstClients = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnClearSelectedState = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(224, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "聊天内容:";
+            // 
+            // txtSendMsg
+            // 
+            this.txtSendMsg.Location = new System.Drawing.Point(133, 297);
+            this.txtSendMsg.Name = "txtSendMsg";
+            this.txtSendMsg.Size = new System.Drawing.Size(305, 21);
+            this.txtSendMsg.TabIndex = 22;
+            this.txtSendMsg.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSendMsg_KeyDown);
+            // 
+            // btnSendMsg
+            // 
+            this.btnSendMsg.Location = new System.Drawing.Point(454, 293);
+            this.btnSendMsg.Name = "btnSendMsg";
+            this.btnSendMsg.Size = new System.Drawing.Size(75, 23);
+            this.btnSendMsg.TabIndex = 21;
+            this.btnSendMsg.Text = "发送信息";
+            this.btnSendMsg.UseVisualStyleBackColor = true;
+            this.btnSendMsg.Click += new System.EventHandler(this.btnSendMsg_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(227, 60);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMsg.Size = new System.Drawing.Size(302, 219);
+            this.txtMsg.TabIndex = 19;
+            // 
+            // btnStartService
+            // 
+            this.btnStartService.Location = new System.Drawing.Point(59, 256);
+            this.btnStartService.Name = "btnStartService";
+            this.btnStartService.Size = new System.Drawing.Size(68, 23);
+            this.btnStartService.TabIndex = 15;
+            this.btnStartService.Text = "启动服务";
+            this.btnStartService.UseVisualStyleBackColor = true;
+            this.btnStartService.Click += new System.EventHandler(this.btnStartService_Click);
+            // 
+            // lstClients
+            // 
+            this.lstClients.FormattingEnabled = true;
+            this.lstClients.ItemHeight = 12;
+            this.lstClients.Location = new System.Drawing.Point(4, 29);
+            this.lstClients.Name = "lstClients";
+            this.lstClients.Size = new System.Drawing.Size(142, 112);
+            this.lstClients.TabIndex = 24;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(62, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 12);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "服务端:";
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.ForeColor = System.Drawing.Color.Red;
+            this.lblIP.Location = new System.Drawing.Point(35, 15);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(0, 12);
+            this.lblIP.TabIndex = 27;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(139, 256);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(69, 21);
+            this.btnExit.TabIndex = 29;
+            this.btnExit.Text = "退出服务";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Port:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "IP:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblPort);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lblIP);
+            this.groupBox1.Location = new System.Drawing.Point(56, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(152, 60);
+            this.groupBox1.TabIndex = 30;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "服务端";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lblPort.Location = new System.Drawing.Point(41, 36);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(0, 12);
+            this.lblPort.TabIndex = 28;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnClearSelectedState);
+            this.groupBox2.Controls.Add(this.lstClients);
+            this.groupBox2.Location = new System.Drawing.Point(56, 101);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(152, 150);
+            this.groupBox2.TabIndex = 31;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "客户端列表";
+            // 
+            // btnClearSelectedState
+            // 
+            this.btnClearSelectedState.Location = new System.Drawing.Point(77, -1);
+            this.btnClearSelectedState.Name = "btnClearSelectedState";
+            this.btnClearSelectedState.Size = new System.Drawing.Size(69, 19);
+            this.btnClearSelectedState.TabIndex = 25;
+            this.btnClearSelectedState.Text = "取消选择";
+            this.btnClearSelectedState.UseVisualStyleBackColor = true;
+            this.btnClearSelectedState.Click += new System.EventHandler(this.btnClearSelectedState_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(306, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "清空消息";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 354);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSendMsg);
+            this.Controls.Add(this.btnSendMsg);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnStartService);
+            this.Name = "FServer";
+            this.Text = "服务端";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSendMsg;
+        private System.Windows.Forms.Button btnSendMsg;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnStartService;
+        private System.Windows.Forms.ListBox lstClients;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnClearSelectedState;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ChatServer3/ChatServer/FServer.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+
+namespace ChatServer
+{
+    public partial class FServer : Form
+    {
+
+        //private HLBeginEndMarkReceiveFilter receiveFilter = new HLBeginEndMarkReceiveFilter();
+        public FServer()
+        {
+            InitializeComponent();
+            //关闭对文本框的非法线程操作检查
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+        }
+        //分别创建一个监听客户端的线程和套接字
+        Thread threadWatch = null;
+        Socket socketWatch = null;
+
+        public const int SendBufferSize = 2 * 1024;
+        public const int ReceiveBufferSize = 1024 * 1024;
+
+
+        private void btnStartService_Click(object sender, EventArgs e)
+        {
+            //定义一个套接字用于监听客户端发来的信息  包含3个参数(IP4寻址协议,流式连接,TCP协议)
+            socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //发送信息 需要1个IP地址和端口号
+            //获取服务端IPv4地址
+            IPAddress ipAddress = GetLocalIPv4Address();
+            lblIP.Text = ipAddress.ToString();
+            //给服务端赋予一个端口号
+            int port = 8088;
+            lblPort.Text = port.ToString();
+
+            //将IP地址和端口号绑定到网络节点endpoint上 
+            IPEndPoint endpoint = new IPEndPoint(ipAddress, port);
+            //将负责监听的套接字绑定网络端点
+            socketWatch.Bind(endpoint);
+            //将套接字的监听队列长度设置为20
+            socketWatch.Listen(20);
+            //创建一个负责监听客户端的线程 
+            threadWatch = new Thread(WatchConnecting);
+            //将窗体线程设置为与后台同步
+            threadWatch.IsBackground = true;
+            //启动线程
+            threadWatch.Start();
+            txtMsg.AppendText("服务器已经启动,开始监听客户端传来的信息!" + "\r\n");
+            btnStartService.Enabled = false;
+        }
+
+        /// <summary>
+        /// 获取本地IPv4地址
+        /// </summary>
+        /// <returns>本地IPv4地址</returns>
+        public IPAddress GetLocalIPv4Address()
+        {
+            IPAddress localIPv4 = null;
+            //获取本机所有的IP地址列表
+            IPAddress[] ipAddressList = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress ipAddress in ipAddressList)
+            {
+                //判断是否是IPv4地址
+                if (ipAddress.AddressFamily == AddressFamily.InterNetwork) //AddressFamily.InterNetwork表示IPv4 
+                {
+                    localIPv4 = ipAddress;
+                }
+                else
+                    continue;
+            }
+            return localIPv4;
+        }
+
+        //用于保存所有通信客户端的Socket
+        Dictionary<string, Socket> dicSocket = new Dictionary<string, Socket>();
+
+        //创建与客户端建立连接的套接字
+        Socket socConnection = null;
+        string clientName = null; //创建访问客户端的名字
+        IPAddress clientIP; //访问客户端的IP
+        int clientPort; //访问客户端的端口号
+        /// <summary>
+        /// 持续不断监听客户端发来的请求, 用于不断获取客户端发送过来的连续数据信息
+        /// </summary>
+        private void WatchConnecting()
+        {
+            while (true)
+            {
+                try
+                {
+                    socConnection = socketWatch.Accept();
+                }
+                catch (Exception ex)
+                {
+                    txtMsg.AppendText(ex.Message); //提示套接字监听异常
+                    break;
+                }
+                //获取访问客户端的IP
+                clientIP = (socConnection.RemoteEndPoint as IPEndPoint).Address;
+                //获取访问客户端的Port
+                clientPort = (socConnection.RemoteEndPoint as IPEndPoint).Port;
+                //创建访问客户端的唯一标识 由IP和端口号组成 
+                clientName = "IP:" + clientIP + ":" + clientPort;
+                lstClients.Items.Add(clientName); //在客户端列表添加该访问客户端的唯一标识
+                dicSocket.Add(clientName, socConnection); //将客户端名字和套接字添加到添加到数据字典中
+
+                //创建通信线程 
+                ParameterizedThreadStart pts = new ParameterizedThreadStart(ServerRecMsg);
+                Thread thread = new Thread(pts);
+                thread.IsBackground = true;
+                //启动线程
+                thread.Start(socConnection);
+                txtMsg.AppendText("IP: " + clientIP + " Port: " + clientPort + " 的客户端与您连接成功,现在你们可以开始通信了...\r\n");
+            }
+        }
+
+        /// <summary>
+        /// 发送信息到客户端的方法
+        /// </summary>
+        /// <param name="sendMsg">发送的字符串信息</param>
+        private void ServerSendMsg(string sendMsg)
+        {
+            sendMsg = txtSendMsg.Text.Trim();
+            //将输入的字符串转换成 机器可以识别的字节数组
+            byte[] arrSendMsg = Encoding.UTF8.GetBytes(sendMsg);
+            //向客户端列表选中的客户端发送信息
+            if (!string.IsNullOrEmpty(lstClients.Text.Trim()))
+            {
+                //获得相应的套接字 并将字节数组信息发送出去
+                dicSocket[lstClients.Text.Trim()].Send(arrSendMsg);
+                //通过Socket的send方法将字节数组发送出去
+                txtMsg.AppendText("您在 " + GetCurrentTime() + " 向 IP:" + clientIP + " Port:" + clientPort + " 的客户端发送了:\r\n" + sendMsg + "\r\n");
+            }
+            else //如果未选择任何客户端 则默认为群发信息
+            {
+                //遍历所有的客户端
+                for (int i = 0; i < lstClients.Items.Count; i++)
+                {
+                    dicSocket[lstClients.Items[i].ToString()].Send(arrSendMsg);
+                }
+                txtMsg.AppendText("您在 " + GetCurrentTime() + " 群发了信息:\r\n" + sendMsg + " \r\n");
+            }
+        }
+
+        string strSRecMsg = null;
+        /// <summary>
+        /// 接收客户端发来的信息
+        /// </summary>
+        private void ServerRecMsg(object socketClientPara)
+        {
+            Socket socketServer = socketClientPara as Socket;
+           
+            while (true)
+            {
+                int firstReceived = 0;
+                byte[] buffer = new byte[ReceiveBufferSize];
+                //获取当前接收到客户端消息IP
+                string clientIp = "IP:" + socketServer.RemoteEndPoint;
+                try
+                {
+                    //获取接收的数据,并存入内存缓冲区  返回一个字节数组的长度
+                    if (socketServer != null) firstReceived = socketServer.Receive(buffer);
+
+                    if (firstReceived > 0) //接受到的长度大于0 说明有信息或文件传来
+                    {
+                        strSRecMsg = Encoding.UTF8.GetString(buffer, 0, firstReceived);//真实有用的文本信息要比接收到的少1(标识符)
+                        txtMsg.AppendText("" + GetCurrentTime() + "收到客户端信息:\r\n" + strSRecMsg + "\r\n");
+                        //receiveFilter.processMatchedRequest(buffer, 0, firstReceived);
+                        ServerSendMsgAuto(clientIp);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //txtMsg.AppendText("系统异常消息:" + ex.Message);
+                    txtMsg.AppendText("客户端" + socketServer.RemoteEndPoint + "已经中断连接" + "\r\n"); //提示套接字监听异常 
+                    lstClients.Items.Remove(clientIp);//从listbox中移除断开连接的客户端
+                    socketServer.Close();//关闭之前accept出来的和客户端进行通信的套接字
+                    dicSocket.Remove(clientIp);
+                    break;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 自动回复信息到客户端的方法
+        /// </summary>
+        /// <param name="clientIp">发送的客户端ip地址</param>
+        private void ServerSendMsgAuto(string clientIp)
+        {
+            string sendMsg = "7E00007E";
+            //将输入的字符串转换成 机器可以识别的字节数组
+            byte[] arrSendMsg = Encoding.UTF8.GetBytes(sendMsg);
+            //向客户端发送字节数组信息
+            dicSocket[clientIp].Send(arrSendMsg);
+            //将发送的字符串信息附加到文本框txtMsg上
+            txtMsg.AppendText("" + GetCurrentTime() + "向客户端<" + clientIp + ">回执:\r\n" + sendMsg + "\r\n");
+
+        }
+
+
+
+        //将信息发送到到客户端
+        private void btnSendMsg_Click(object sender, EventArgs e)
+        {
+            ServerSendMsg(txtSendMsg.Text);
+        }
+
+        //快捷键 Enter 发送信息
+        private void txtSendMsg_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ServerSendMsg(txtSendMsg.Text);
+            }
+        }
+
+        /// <summary>
+        /// 获取当前系统时间
+        /// </summary>
+        public DateTime GetCurrentTime()
+        {
+            DateTime currentTime = new DateTime();
+            currentTime = DateTime.Now;
+            return currentTime;
+        }
+
+        //关闭服务端
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //取消客户端列表选中状态
+        private void btnClearSelectedState_Click(object sender, EventArgs e)
+        {
+            lstClients.SelectedItem = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtMsg.Text = "";
+        }
+    }
+}
